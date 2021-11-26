1ff33cf5edff529b74be579dc7c0daffa72ea8c9
tri <ngoctri2207@gmail.com>
2021-11-26 23:00:37 +0700

    add

--- components/organisms/experience/index.js
+++ components/organisms/experience/index.js
@@ -1,23 +1,25 @@
+import React from "react";
 import { SiWebmoney } from "react-icons/si";
-const Card = ({startYear,endYear,title,company, description}) => {
+const Card = ({ startYear, endYear, title, company, description }) => {
   return (
     <div className="timeline">
       <a href="#" className="timeline-content">
         <div className="timeline-icon">
           <SiWebmoney />
         </div>
-        <div className="timeline-year"><p>{startYear}</p> <p>-</p> <p>{endYear}</p></div>
-        
+        <div className="timeline-year">
+          <p>{startYear}</p> <p>-</p> <p>{endYear}</p>
+        </div>
+
         <h3 className="title">{title}</h3>
         <p className="company">{company}</p>
         <p className="description">{description}</p>
       </a>
     </div>
-    
   );
 };
-export default function Experience({ data }) {
-  const dataExperience = data.experience
+const Experience = ({ data }) => {
+  const dataExperience = data.experience;
   return (
     <div className="experience">
       <div className="container" id="resum">
@@ -26,7 +28,16 @@ export default function Experience({ data }) {
             <h2 className="title">Experience</h2>
             <div className="main-timeline">
               {dataExperience.map((item, index) => {
-                return <Card key={index} startYear={item.startYear} endYear={item.endYear} company={item.company} description={item.description} title={item.title}/>;
+                return (
+                  <Card
+                    key={index}
+                    startYear={item.startYear}
+                    endYear={item.endYear}
+                    company={item.company}
+                    description={item.description}
+                    title={item.title}
+                  />
+                );
               })}
             </div>
           </div>
@@ -34,4 +45,5 @@ export default function Experience({ data }) {
       </div>
     </div>
   );
-}
+};
+export default Experience;
