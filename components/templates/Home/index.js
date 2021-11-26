a49e3ee360e404f5ca18f292da793064d464f9f2
tri <ngoctri2207@gmail.com>
2021-11-26 23:11:53 +0700

    a

--- components/templates/Home/index.js
+++ components/templates/Home/index.js
@@ -1,13 +1,13 @@
 import About from "../../organisms/About";
-import Experience from "../../organisms/Experience";
+// import Experience from "../../organisms/Experience";
 import Skill from "../../organisms/Skill/index";
 import Project from "../../organisms/Project";
-import data from "./data.json";
+// import data from "./data.json";
 export default function Home() {
   return (
     <div>
       <About />
-      <Experience data={data} />
+      {/* <Experience data={data} /> */}
       <Skill />
       <Project />
     </div>
