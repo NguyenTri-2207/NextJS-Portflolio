f95055c7e97bed05c90761fe6f791b451243cec3
TriNguyen <tri.nguyen@gleads.vn>
2022-12-02 15:17:12 +0700

    add

--- components/organisms/Banner/index.js
+++ components/organisms/Banner/index.js
@@ -3,29 +3,29 @@ import PropTypes from "prop-types";
 import Image from "next/image";
 const Banner = (props) => {
   return (
-    <div className="w-full h-[calc(100vh-80px)] pl-[84px]  relative">
-      <div className="container h-full">
-        <div className="row items-center h-full">
-          <div className="lg:col-6 ">
-            <p className="text-main lg:text-lg  relative font-poppins font-semibold  before:content-[''] before:h-0.5 before:w-6 before:bg-main before:absolute before:bottom-[14px] before:-left-10">
+    <div className="w-full lg:h-[calc(100vh-80px)] lg:pl-[84px]  relative">
+      <div className="container lg:h-full">
+        <div className="row items-center lg:h-full">
+          <div className="lg:col-6 pl-10 mt-[160px] lg:mt-0">
+            <p className="text-main lg:text-lg  relative font-poppins font-semibold  before:content-[''] before:h-0.5 lg:before:w-6 before:w-2 before:bg-main before:absolute before:bottom-[14px] before:-left-4 lg:before:-left-10">
               HELLO DEAR
             </p>
-            <h1 className="text-white text-4xl lg:leading-[120px] lg:text-[60px] font-Playfair font-bold">
+            <h1 className="text-white text-4xl leading-[80px] lg:leading-[120px] lg:text-[60px] font-Playfair font-bold">
               I’m Tri Nguyen
             </h1>
-            <h1 className="text-main text-4xl lg:leading-[50px] lg:text-[60px] font-Playfair font-bold">
+            <h1 className="text-main text-4xl leading-[30px] lg:leading-[50px] lg:text-[60px] font-Playfair font-bold">
               Web Developer
             </h1>
             <button></button>
           </div>
-          <div className="lg:col-6 m-auto text-center relative">
+          <div className="lg:col-6 lg:m-auto text-center relative mt-6 mb-16 lg:my-0">
             <Image
               src="/assets/banner/avt2.png"
               width={243}
               height={243}
               alt="bg"
             />
-            <div className="absolute -top-20 -left-20 animate-pulse">
+            <div className="absolute -top-20 -left-20 animate-pulse hidden lg:block">
               <Image
                 src="/assets/banner/bg-mt.png"
                 width={128}
@@ -33,7 +33,7 @@ const Banner = (props) => {
                 alt="bg"
               />
             </div>
-            <div className="absolute -bottom-20 -left-6 animate-pulse">
+            <div className="absolute -bottom-20 -left-6 animate-pulse hidden lg:block">
               <Image
                 src="/assets/banner/bg-m.png"
                 width={88}
@@ -45,7 +45,7 @@ const Banner = (props) => {
         </div>
       </div>
 
-      <div className="absolute -bottom-2  left-16">
+      <div className="absolute -bottom-2  left-16 hidden lg:block">
         <Image
           src="/assets/banner/bg-lb.png"
           width={320}
@@ -53,7 +53,7 @@ const Banner = (props) => {
           alt="bg"
         />
       </div>
-      <div className="absolute top-0 lg:right-20 right-1">
+      <div className="absolute top-0 lg:right-20 right-1 ">
         <Image
           src="/assets/banner/bg-t.png"
           width={384}
@@ -61,7 +61,7 @@ const Banner = (props) => {
           alt="bg"
         />
       </div>
-      <div className="absolute -bottom-2  -right-6 rotate-[268deg]">
+      <div className="absolute -bottom-2  -right-6 rotate-[268deg] hidden lg:block">
         <Image
           src="/assets/banner/bg-lb.png"
           width={320}
--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   trailingSlash: true,
   sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
+    // includePaths: [path.join(__dirname, "styles")],
   },
 
   exportPathMap: async function (
