bd60fc81a5d4a34140cba6b0d1baef5643ec42a3
Tri Nguyen <tri.nguyen@gleads.vn>
2023-06-12 11:59:37 +0700

    add

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "styles/globals.scss";
+import Head from "next/head";
 // import "bootstrap/dist/css/bootstrap.min.css";
 import { ThemContext } from "../common/context";
 const dark = "#343a40";
@@ -7,17 +8,26 @@ const light = "#f8f9fa";
 function MyApp({ Component, pageProps }) {
   const [theme, setTheme] = useState(false);
   return (
-    <ThemContext.Provider value={{ theme, setTheme }}>
-      <div
-        className="font-poppinss overflow-hidden"
-        style={{
-          color: `${!theme ? light : dark}`,
-          background: `${theme ? light : dark}`,
-        }}
-      >
-        <Component {...pageProps} />
-      </div>
-    </ThemContext.Provider>
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+      </Head>
+      <ThemContext.Provider value={{ theme, setTheme }}>
+        <div
+          className="font-poppinss overflow-hidden"
+          style={{
+            color: `${!theme ? light : dark}`,
+            background: `${theme ? light : dark}`,
+          }}
+        >
+          <Component {...pageProps} />
+        </div>
+      </ThemContext.Provider>
+    </>
   );
 }
 
