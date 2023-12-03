fa55edfec83155ff27b55d7896d5d165bef8f2e3
tri <ngoctri2207@gmail.com>
2023-12-03 17:26:56 +0700

    update content

--- components/templates/AboutTemplate/index.js
+++ components/templates/AboutTemplate/index.js
@@ -45,7 +45,7 @@ const Card = ({ startYear, endYear, title, company, description }) => {
           {startYear + " - " + endYear}
         </span>
         <h3 className="text-lg dark:text-white">{title}</h3>
-        <p className="dark:text-[#b7b7b7] mb-2 text-sm">{description}</p>
+        <p className="dark:text-[#b7b7b7] mb-2 text-sm line-clamp-3">{description}</p>
         <p className="dark:text-white text-xs italic">{company}</p>
       </div>
     </div>
@@ -68,16 +68,20 @@ const About = () => {
               Front-End Developer
             </span>
           </div>
-          <div className=" pb-7 border-b border-gray-700 dark:border-gray-50 dark:text-gray-300">
+          <div className=" pb-7 border-b border-gray-700 dark:border-gray-50 dark:text-gray-300 text-sm">
             <p className="mb-3 leading-7 ">
-              I{`'`}m a Web Developer, I am passionate and dedicated to my work.
-              With the knowledge I learned in college and self-study at home.
+              I'm a Software Engineer with 3+ years of experience React and state management like Redux with proficiency in web technologies like HTML5, CSS3, JavaScript, building large-scale web applications, cross-browser platforms, and design constraints on the web, and building responsive user interfaces.
             </p>
-            <p className="leading-7 ">
-              I gained the skills and knowledge needed to create a website
-              interface. I am trying every day to improve my skills to become a
-              senior front end developer
-            </p>
+            <ul>
+              <li className=" mb-2">✨ Version control system (GIT), Defect Tracking tool (Jira,Clickup), design platform (Figma), and testing framework such as Jet.
+              </li>
+              <li className=" mb-2">            ✨ Languages: React, Typescript, JavaScript, NodeJS.
+              </li>
+              <li className=" mb-2">✨ Web technology : HTML5, CSS3, SASS</li>
+              <li className=" mb-2">✨ Bootsrap4, TailwindCSS</li>
+              <li className=" mb-2">✨ Databases: MongoDB</li>
+              <li className=" mb-2">✨Other Tools: Performance Optimization Tools, Cross-Browser Testing: BrowserStack, Web Hosting Platforms: Vercel, AWS, Content Management Systems (CMS):WordPress.</li>
+            </ul>
           </div>
           <h3 className="text-2xl font-medium mt-6 mb-4 dark:text-white">
             {" "}
@@ -99,19 +103,21 @@ const About = () => {
           </div>
         </div>
       </div>
-      {theme && (
-        <>
+      {
+        theme && (
+          <>
 
-          <img
-            className="absolute right-0 top-0 z-0 lg:block hidden  "
-            src="/assets/bg-about-tr.png"
-            width={206}
-            height={313}
-            alt="a"
-          />
-        </>
-      )}
-    </div>
+            <img
+              className="absolute right-0 top-0 z-0 lg:block hidden  "
+              src="/assets/bg-about-tr.png"
+              width={206}
+              height={313}
+              alt="a"
+            />
+          </>
+        )
+      }
+    </div >
   );
 };
 export default About;
