f24b45b025e9f7552505e335d7931ffaeb3cecfb
tri <ngoctri2207@gmail.com>
2023-12-16 10:03:10 +0700

    fix content

--- components/templates/AboutTemplate/index.js
+++ components/templates/AboutTemplate/index.js
@@ -62,7 +62,7 @@ const About = () => {
             Who am i?
           </div>
           <div className=" text-2xl leading-10 lg:font-medium dark:text-gray-300">
-            I{`'`}m Tri and
+            I&apos;m Tri and
             <span className="dark:text-yellow lg:font-medium">
               {" "}
               Front-End Developer
@@ -70,7 +70,7 @@ const About = () => {
           </div>
           <div className=" pb-7 border-b border-gray-700 dark:border-gray-50 dark:text-gray-300 text-sm">
             <p className="mb-3 leading-7 ">
-              I'm a Software Engineer with 3+ years of experience React and state management like Redux with proficiency in web technologies like HTML5, CSS3, JavaScript, building large-scale web applications, cross-browser platforms, and design constraints on the web, and building responsive user interfaces.
+              I&apos;m a Software Engineer with 3+ years of experience React and state management like Redux with proficiency in web technologies like HTML5, CSS3, JavaScript, building large-scale web applications, cross-browser platforms, and design constraints on the web, and building responsive user interfaces.
             </p>
             <ul>
               <li className=" mb-2">✨ Version control system (GIT), Defect Tracking tool (Jira,Clickup), design platform (Figma), and testing framework such as Jet.
@@ -84,7 +84,7 @@ const About = () => {
             </ul>
           </div>
           <h3 className="text-2xl font-medium mt-6 mb-4 dark:text-white">
-            {" "}
+
             What I do!
           </h3>
           <div className="lg:grid grid-cols-2 lg:grid-cols-2 gap-x-6 gap-y-2 mt-[30px]">
